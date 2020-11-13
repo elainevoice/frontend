e36ecb406db6c8fdbeeab4a76ad8e534dd94f65a
Quirijn Langedijk <github@quirijnlangedijk.nl>
2020-11-13 12:21:55 +0100

    Record je mooie stemmetje

--- src/app/pages/formpage/FormPage.tsx
+++ src/app/pages/formpage/FormPage.tsx
@@ -1,34 +1,38 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
-import { BsFillPlayFill, BsFillPauseFill } from "react-icons/bs";
-import { BiMicrophone, BiMicrophoneOff } from "react-icons/bi";
+import { BsFillPlayFill, BsFillPauseFill } from 'react-icons/bs';
+import { BiMicrophone, BiMicrophoneOff } from 'react-icons/bi';
 
-import "./FormPage.scss";
+import './FormPage.scss';
 
-import SineWave from "../../components/hero/SineWave";
+import SineWave from '../../components/hero/SineWave';
+import { Record } from '../../components/record/Record';
 
 export default class FormPage extends Component<{}, {}> {
     render() {
         return (
             <div className="body">
-                {/* Test */}
+                <Row>
+                    <Record />
+                </Row>
+
+                {/* Test 
                 <div className="hero">
                     <SineWave/>
                 </div>
-                {/* ./Test */}
-
-                <Row className="justify-content-md-center">
+                 ./Test */}
+                {/*<Row className="justify-content-md-center">
                     <Button variant="link">
                         <BsFillPlayFill/>
                     </Button>
                     <h1>00:16</h1>
-                </Row>
-                <Form>
+        </Row>*/}
+                {/*<Form>
                     <Row className="justify-content-md-center">
                         <Col md="6">
                             <Form.Group>
@@ -58,9 +62,8 @@ export default class FormPage extends Component<{}, {}> {
                             </Form.Group>
                         </Col>
                     </Row>
-                </Form>
+                </Form>*/}
             </div>
         );
     }
 }
-    
